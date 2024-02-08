73eb852279e81928ddf690cacc72b94bc056985b
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-08 19:57:11 +0900

    msm8974-common: Go back to original wifi hal

--- common.mk
+++ common.mk
@@ -313,7 +313,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     hostapd \
     libnetcmdiface \
     libwpa_client \
